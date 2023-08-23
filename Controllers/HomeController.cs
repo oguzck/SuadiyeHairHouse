0137aa94c2ef3bf8590418fb503bc32187631c51
oguzk <oguzcankilickaya@gmail.com>
2023-08-23 20:56:31 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace SuadiyeHairHouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult BeforeAndAfter()
+        {
+            return View();
+        }
+
+
+    }
+}
