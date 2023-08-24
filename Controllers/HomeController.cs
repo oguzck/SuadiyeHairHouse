2cb7ac9a5bdf823889f4545b7dfa666e9847fd86
oguzk <oguzcankilickaya@gmail.com>
2023-08-24 23:03:05 +0300

    SSS eklendi , footer düzenlendi , contact sayfası yapıldı  , newlettter düzenlendi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,10 @@ namespace SuadiyeHairHouse.Controllers
         {
             return View();
         }
+        public IActionResult FrequentlyAskedQuestions()
+        {
+            return View();
+        }
 
 
     }
