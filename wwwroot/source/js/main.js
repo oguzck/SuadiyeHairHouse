e43c4936adbfadc40bb8f33b2b640db7d6796209
oguzk <oguzcankilickaya@gmail.com>
2023-09-02 11:06:35 +0300

    js changed

--- wwwroot/source/js/main.js
+++ wwwroot/source/js/main.js
@@ -62,9 +62,10 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 600, 'easeInOutExpo'); 
         return false;
     });
+
 
 
     // Date and time picker
